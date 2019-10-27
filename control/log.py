27d6edf040ca15a60b9fc7c5be50efb92dd8cd46
Hsu Chih-Ming <hsu.chihming@gmail.com>
2019-10-27 21:44:42 +0800

    - use global variable to enable/disable 'log' feature

--- control/log.py
+++ control/log.py
@@ -7,35 +7,26 @@ from datetime import datetime
 
 
 def init(arg_enable=0):
-    os.environ["USIM_MODIFIER_LOG"] = str(arg_enable)
+    global log_enable
+    
+    log_enable = arg_enable
 
-    if log_enable():
+    if log_enable:
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name)-16s %(levelname)-8s %(message)s',
                             datefmt='%m-%d %H:%M:%S',
                             handlers=[logging.FileHandler(datetime.today().strftime("%Y%m%d-%H%M%S.log"), 'w', 'utf-8'), ])
 
-
-def log_enable():
-    try:
-        if os.environ["USIM_MODIFIER_LOG"] == "1":
-            return True
-    except:
-        pass
-
-    return False
-
-
 def info(arg_tag, arg_message):
-    if log_enable():
+    if log_enable:
         logging.getLogger(arg_tag).info(arg_message)
 
 
 def debug(arg_tag, arg_message):
-    if log_enable():
+    if log_enable:
         logging.getLogger(arg_tag).debug(arg_message)
 
 
 def critical(arg_tag, arg_message):
-    if log_enable():
+    if log_enable:
         logging.getLogger(arg_tag).critical(arg_message)
