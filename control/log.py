0fb9ed3341a579d57e96c7f174c5a393ffec3c45
Hsu Chih-Ming <hsu.chihming@gmail.com>
2020-03-08 13:01:24 +0800

    - move the log files to 'logs' folder

--- control/log.py
+++ control/log.py
@@ -5,17 +5,23 @@ import os
 import logging
 from datetime import datetime
 
+DEF_LOG_FOLDER_NAME = "logs"
+
 
 def init(arg_enable=0):
     global log_enable
-    
+
     log_enable = arg_enable
 
     if log_enable:
+        if not os.path.exists(DEF_LOG_FOLDER_NAME):
+            os.makedirs(DEF_LOG_FOLDER_NAME)
+
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name)-16s %(levelname)-8s %(message)s',
                             datefmt='%m-%d %H:%M:%S',
-                            handlers=[logging.FileHandler(datetime.today().strftime("%Y%m%d-%H%M%S.log"), 'w', 'utf-8'), ])
+                            handlers=[logging.FileHandler(datetime.today().strftime(DEF_LOG_FOLDER_NAME + os.sep + "%Y%m%d-%H%M%S.log"), 'w', 'utf-8'), ])
+
 
 def info(arg_tag, arg_message):
     if log_enable:
